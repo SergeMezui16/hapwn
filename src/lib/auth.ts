00caed92f70091d953d15e3878e059458c215c9e
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-05-05 22:55:54 +0100

    Add: login credential

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,33 @@
 import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import {signInSchema} from "@/lib/zod";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+
+
+        const { email, password } = await signInSchema.parseAsync(credentials);
+
+        // logic to verify if user exists
+        const user = {username: "Serge", email}
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.")
+        }
+
+        // return user object with their profile data
+        return user
+      },
+    }),
+  ],
 })
