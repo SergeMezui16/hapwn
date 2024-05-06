6f5333ec668b9587528c37932263013b2570bf74
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-05-06 23:01:24 +0100

    Fix: protected admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,4 @@
+export default function Page()
+{
+  return <>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi id ipsum minus nihil, non tenetur? Commodi consectetur cumque delectus facilis fugit maxime omnis quis quod recusandae rerum, sed vel, voluptas.</>
+}
