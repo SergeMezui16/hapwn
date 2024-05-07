fe962c511e23433a7b87c785e21345ac7766bb8f
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-05-07 22:57:55 +0100

    Fix: disallow password

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -6,6 +6,6 @@ export const signInSchema = object({
     .email("Invalid email"),
   password: string({ required_error: "Password is required" })
     .min(1, "Password is required")
-    .min(8, "Password must be more than 8 characters")
+    // .min(8, "Password must be more than 8 characters")
     .max(32, "Password must be less than 32 characters"),
 })
